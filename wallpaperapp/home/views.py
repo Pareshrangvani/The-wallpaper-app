18946c88ac32fa33e3aa94d97b864bcadea78a3b
Pareshrangvani <rangvaniparesh@gmail.com>
2021-03-09 02:57:17 +0530

    first commit

---
+++ wallpaperapp/home/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    # return HttpResponse('Hello')
+    context = {
+        'variable' : "this is variable text"
+    }
+    return render(request, 'index.html', context)
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
